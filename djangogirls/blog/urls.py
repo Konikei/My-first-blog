cc953723b7f6ab32dfd10c4b45b0a537b41535d8
Alex <alex.applegate79@gmail.com>
2023-05-10 17:25:55 -0400

    Tried adding the new url, probably failed

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from . import views
 from django.shortcuts import render
+from .models import Post
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='postdetail'),
+    path('hello/', views.hello, name='hello'),
 ]
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_detail(request, pk):
+    # Retrieve the post object with the given primary key (pk)
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    print("post_detail view function called with pk =", pk)
+    # Retrieve the post object with the given primary key (pk)
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse('Hello, world!')
+
+
